de1b2edc5f9fe538208b64398116833c59189e2e
Nicolas Battisti <nickgabrielbd@gmail.com>
2024-02-07 14:42:20 -0300

    feat: resolver resolve var declarations

--- src/interpreter/resolver.py
+++ src/interpreter/resolver.py
@@ -1,8 +1,9 @@
 
 from typing import Dict, List
-from .statements import Block, Stmt, StmtVisitor
+from .statements import Block, Stmt, StmtVisitor, Var
 from .expression import Expr, ExprVisitor
 from .interpreter import Interpreter
+from ..lexer.tokens import Token
 
 
 class Resolver(ExprVisitor, StmtVisitor):
@@ -11,20 +12,34 @@ class Resolver(ExprVisitor, StmtVisitor):
         self.__interpreter = interpreter
         self.__scopes: List[Dict[str, bool]] = [] 
 
-    def _begin_scope(self):
+    def _begin_scope(self) -> None:
         self.__scopes.append({})
 
-    def _end_scope(self):
+    def _declare(self, name: Token) -> None:
+        if not self.__scopes:
+            return None
+
+        scope = self.__scopes[-1]
+        scope[name.lexeme] = False
+
+    def _define(self, name: Token) -> None:
+        if not self.__scopes:
+            return None
+
+        self.__scopes[-1][name.lexeme] = True
+
+
+    def _end_scope(self) -> None:
         self.__scopes.pop()
 
-    def _resolve(self, statements: List[Stmt]):
+    def _resolve(self, statements: List[Stmt]) -> None:
         for stmt in statements:
             self._resolve_stmt(stmt)
 
-    def _resolve_stmt(self, stmt: Stmt):
+    def _resolve_stmt(self, stmt: Stmt) -> None:
             stmt.accept(self)
 
-    def _resolve_expr(self, expression: Expr):
+    def _resolve_expr(self, expression: Expr) -> None:
         expression.accept(self)
 
     def visit_block_stmt(self, stmt: Block) -> None:
@@ -32,4 +47,11 @@ class Resolver(ExprVisitor, StmtVisitor):
         self._resolve(stmt.statements)
         self._end_scope()
 
+        return None
+
+    def visit_var_stmt(self, stmt: Var) -> None:
+        self._declare(stmt.name)
+        if stmt.initializer is not None:
+            self._resolve_expr(stmt.initializer)
+        self._define(stmt.name)
         return None
